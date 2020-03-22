b41394d37614212a0f2c2fb77ad7c827a363d2a9
payroll-beats <prod24@protonmail.com>
2020-03-21 20:46:59 -0400

    added retry option,now you can store output to a file

--- encrypt-decrypt.lua
+++ encrypt-decrypt.lua
@@ -1,27 +1,44 @@
+--[[To Do list
+    add decryption
+    allow directories to be made
+    make improvemtns--]]
+
+
 os.execute("clear")
 
+--Start up the program
+local function encryptionDecryption()
+
+--Question Prompted on run
+os.clock()
 print("Do you want to encypt or decrypt? \n -encrypt \n -decrypt")
+print(math.randomseed(2))
 local response = io.read()
 
+--Selection part 
 if (response == "encrypt" or "-encrypt") then 
 	os.execute("cat cipherAlgorithms.txt")
-	print("Pick an encryption method")
+	print("Pick an encryption method \n")
 	local cipherChoice = io.read()
 	print("Pick a file")
 	os.execute("ls")
 	local fileChoice = io.read()
-	local encryptedFileResponse = os.execute("openssl enc -"..cipherChoice.." -in "..fileChoice)
-	print("Do you want to save this result? \n yes or no")
-	local yesNo = io.read()
-		if(yesNo == "yes") then
-			print("Type in a file name to save encrypted file")
+	local encryptedFileResponse = os.execute("openssl enc -"..cipherChoice.." -in "..fileChoice..">> tmpsslfile")
+	print("\n Do you want to save this result? \n yes or no \n or type retry to go back")
+	local yesNoRetry = io.read()
+	-- If yes then allow for save
+		if(yesNo == "yes" or "-yes") then
+			print("Type in a file name to save encrypted file \n")
 			local fileName = io.read()
-			os.execute("touch "..fileName)
-			print(encryptedFileRepsonse)
+			os.execute("touch "..fileName);os.execute("mv tmpsslfile "..fileName)
+			os.execute("ls");print("Output \n");os.execute("cat "..fileName)
 			io.read()
-	    elseif(encryptedFileResponse == "no") then
-			os.execute("clear")
+	        elseif(yesNo == "no" or "-no") then
+			os.execute("bye have a beautiful time")
 			
+	        elseif(yesNo == "retry" or "-retry") then
+		encryptionDecryption()
+		print("yes")
 		end
 
 elseif (response == "decrypt" or "-decrypt") then
@@ -30,4 +47,6 @@ elseif (response == "decrypt" or "-decrypt") then
 else 
 	print("select a valid option")
 end
-
+end
+encryptionDecryption()
+io.read()
